3f6a277e8552d399df090b5f95a727253a17a721
ShanzayHussain <shanzayhussain21212@gmail.com>
2026-05-19 03:38:06 +0500

    .

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,12 @@ require('dotenv').config()
 const app = express()
 
 // ── Middleware ──
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://interview-iq-bay.vercel.app/'
+  ]
+}))
 app.use(express.json())
 
 // ── Routes ──
